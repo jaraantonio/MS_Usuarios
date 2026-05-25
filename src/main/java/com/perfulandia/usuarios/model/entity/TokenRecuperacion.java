79e191cb66028c5c68ddd505b208d5e37e6c5c76
Antonio Jara <anton.jaras@duocuc.cl>
2026-05-25 03:51:50 -0400

    feat: agregar dependencias para JWT y crear TokenInvalidado y TokenRecuperacion

---
+++ src/main/java/com/perfulandia/usuarios/model/entity/TokenRecuperacion.java
@@ -0,0 +1,30 @@
+package com.perfulandia.usuarios.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tokens_recuperacion")
+@Getter
+@Setter
+public class TokenRecuperacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private String correo;
+
+    @Column(nullable = false)
+    private LocalDateTime expiracion;
+}
